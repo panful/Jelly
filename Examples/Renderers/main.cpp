af8fa12b517652e799c7d2eb0949f7ea2056e058
worker-Windows <yangpan0822@qq.com>
2024-06-24 14:21:02 +0800

    添加 Renderers 示例

---
+++ Examples/Renderers/main.cpp
@@ -0,0 +1,27 @@
+#include "InteractorGLFW.h"
+#include "Renderer.h"
+#include "WindowGLFW.h"
+#include <iostream>
+#include <memory>
+
+int main()
+{
+    auto renderer1 = std::make_shared<Jelly::Renderer>();
+    renderer1->SetBackground({.2f, .2f, .2f, 1.f});
+    renderer1->SetViewport({.05, .05, .4, .4});
+
+    auto renderer2 = std::make_shared<Jelly::Renderer>();
+    renderer2->SetBackground({.3f, .3f, .3f, 1.f});
+    renderer2->SetViewport({.55, .55, .4, .4});
+
+    auto window = std::make_shared<Jelly::WindowGLFW>();
+    window->AddRenderer(renderer1);
+    window->AddRenderer(renderer2);
+    window->SetSize(800, 600);
+    window->SetTitle("Test window");
+    window->Render();
+
+    auto interactor = std::make_unique<Jelly::InteractorGLFW>();
+    interactor->SetWindow(window);
+    interactor->Start();
+}
